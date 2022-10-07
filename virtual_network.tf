3b455a351ec91b0ba40582d629b6cf472a0580d0
Linus Weigand <ge63nat@mytum.de>
2022-10-07 17:03:02 +0200

    subnet

--- virtual_network.tf
+++ virtual_network.tf
@@ -5,3 +5,34 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   resource_group_name = azurerm_resource_group.test-rg.name
 }
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.test-rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+resource "azurerm_public_ip" "publicip" {
+  name                = "pip1"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test-rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+# Network interface
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.prefix}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test-rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+}
