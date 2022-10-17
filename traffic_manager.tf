691e3d4b0054762c7d178f7ac39c08468a4e3963
Linus Weigand <ge63nat@mytum.de>
2022-10-17 15:47:15 +0200

    traffic_manager

---
+++ traffic_manager.tf
@@ -0,0 +1,33 @@
+# Traffic Manager for Java Web App
+resource "azurerm_traffic_manager_profile" "traffic_manager" {
+  name                   = "${var.prefix}-java-webapp-tm"
+  resource_group_name    = azurerm_resource_group.global-prod-rg.name
+  traffic_routing_method = "Performance"
+  dns_config {
+    relative_name = "${var.prefix}-java-webapp"
+    ttl           = 300
+  }
+
+  monitor_config {
+    protocol                      = "http"
+    port                          = 80
+    path                          = "/"
+    interval_in_seconds           = 30
+    timeout_in_seconds            = 9
+    toleraterd_number_of_failures = 3
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+# Endpoint North
+resource "azurerm_traffic_manager_endpoint" "north-tm-enpoint" {
+  name                = "${var.prefix}-north-global-tm"
+  resource_group_name = azurerm_resource_group.global-prod-rg.name
+  profile_name        = azurerm_traffic_manager_profile.traffic_manager.name
+  target              = azurerm_public_ip.north-publicip.fqdn
+  endpoint_location   = azurerm_public_ip.north-publicip.location
+  type                = "externalEndpoints"
+}
